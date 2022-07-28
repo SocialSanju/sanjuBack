139dd8fefbdd5a552377e136e2d05f6406ea92cf
sbframee <sbframee@gmail.com>
2022-07-28 19:21:59 +0530

    updated

--- routers/authorRouter.js
+++ routers/authorRouter.js
@@ -5,8 +5,12 @@ import Author from '../models/authorModel.js';
 const authorRouter = express.Router();
 
 authorRouter.get('/', expressAsyncHandler(async(req, res) => {
-  const authors = await Author.find({});
-  res.send(authors);
+    try{
+        const authors = await Author.find();
+        res.json(authors);
+    }catch(err){
+        res.send('error',err);
+    }
 })
 );
 
